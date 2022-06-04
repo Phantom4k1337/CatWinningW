7da4d9c5b60a1ba4aac04f1312803dd745284f76
Phantom4k1337 <78486283+Phantom4k1337@users.noreply.github.com>
2022-06-04 20:28:20 +0300

    Update ThemesSystem.lua

--- ThemesSystem.lua
+++ ThemesSystem.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'ZeroMonth\CB'
+	ThemeManager.Folder = 'ZeroMonth'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
